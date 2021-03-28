a3b36422fd91f0eb05090541a0bcb5709f3a9c7e
Ali Akbar <Ali_akbar96@hotmail.com>
2021-03-28 13:31:39 -0400

    Adding Files

--- Mapping_Single_Points/static/js/logic.js
+++ Mapping_Single_Points/static/js/logic.js
@@ -6,7 +6,7 @@ let map = L.map('mapid').setView([40.7, -94.5], 4);
 
 
 //  Add a marker to the map for Los Angeles, California.
-L.circleMarker([34.0522, -118.2437], {
+L.circleMarker([34.0522,-118.2437], {
     radius: 100,
     color: "black",
     fillcolor: '#ffffa1'
